5f605234f2521936cac40640f3ac46b95da1da91
StellaEtCorpus <145240056+SuryakandaRagaWistara@users.noreply.github.com>
2025-12-30 12:32:40 +0700

    Fix comment formatting in user model
    Corrected comment formatting for clarity.

--- models/user.go
+++ models/user.go
@@ -6,6 +6,6 @@ type User struct {
     Email     string    `json:"email"`
     Role      string    `json:"role"`
     // Ini tetap ada. Ini bukan kolom di DB, 
-    // melainkan tempat untuk menyimpan hasil join dari tabel Task.
+    //  tempat untuk menyimpan hasil join dari tabel Task.
     Tasks     []Task    `json:"tasks,omitempty"` 
 }
